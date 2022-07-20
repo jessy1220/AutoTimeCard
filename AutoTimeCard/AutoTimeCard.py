ac4331448aa94a6ed18b445c243fec08b3722eb8
longhaox.lee <longhaox.lee@intel.com>
2022-07-20 16:27:38 +0800

    Add import error handling

--- AutoTimeCard/AutoTimeCard.py
+++ AutoTimeCard/AutoTimeCard.py
@@ -1,15 +1,29 @@
 from msilib.schema import RadioButton
 import os
-import tkinter as tk
+#import tkinter as tk
 import time
-from tkinter import messagebox
+#from tkinter import messagebox
 from datetime import datetime
 from datetime import timedelta
 from threading import Timer
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+#from selenium import webdriver
+
+try:
+  import tkinter as tk
+except ImportError:
+  print ("Trying to Install required module: tkinter\n")
+  os.system('py -3 -m pip install tkinter')
+
+try:
+  from selenium.webdriver.common.by import By
+  from selenium.webdriver.support import expected_conditions as EC
+  from selenium.webdriver.support.wait import WebDriverWait
+except ImportError:
+  print ("Trying to Install required module: selenium\n")
+  os.system('py -3 -m pip install selenium')
+  from selenium.webdriver.common.by import By
+  from selenium.webdriver.support import expected_conditions as EC
+  from selenium.webdriver.support.wait import WebDriverWait
 
 try:
   from msedge.selenium_tools import Edge, EdgeOptions
